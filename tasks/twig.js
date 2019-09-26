d91527ca3803d723366382ff25bf4e09ed2c3bcf
sergei kuksov <sergey.kuksov@func.ru>
2019-09-27 01:15:58 +0300

    Add custom filter and custom function

--- tasks/twig.js
+++ tasks/twig.js
@@ -3,8 +3,10 @@ const { src, dest } = require('gulp');
 const plumber = require('gulp-plumber');
 const prettyHtml = require('gulp-pretty-html');
 const twig = require('gulp-twig');
+const data = require('gulp-data');
 const gulpif = require('gulp-if');
 const browserSync = require('browser-sync').create();
+const fs = require('fs');
 const config = require('./config');
 
 const { reload } = browserSync;
@@ -19,9 +21,44 @@ const compileTwig = cb => {
     content_unformatted: []
   };
 
+  // const data = {
+  //   title: 'Gulp and Twig',
+  //   benefits: [
+  //     'Fast',
+  //     'Flexible',
+  //     'Secure'
+  //   ]
+  // };
+
   src(config.pages.input)
     .pipe(plumber(config.notify))
-    .pipe(twig())
+    .pipe(data(function (file) {
+      return JSON.parse(fs.readFileSync('src/static/data/test.json'));
+    }))
+    .pipe(twig({
+      // data: data,
+      // base: '', // views base folder
+      functions: [
+        {
+          name: "customFunction",
+          func: function (args) {
+            console.log(args);
+
+            return "the function";
+          }
+        }
+      ],
+      filters: [
+        {
+          name: "customFilter",
+          func: function (args) {
+            console.log(arguments);
+            
+            return args + " the filter";
+          }
+        }
+      ],
+    }))
     .pipe(gulpif(isDev, prettyHtml(prettyHtmlConfig)))
     .pipe(dest(config.pages.temp))
     .pipe(dest(config.pages.output))
